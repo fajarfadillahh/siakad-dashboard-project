ce4385fded55ed986238c52a8395a597437f7756
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-23 20:57:50 +0700

    create: typography components

---
+++ src/components/typography/PageTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PageTitle({ children }) {
+  return (
+    <h1 className="my-6 text-2xl font-semibold text-gray-900">{children}</h1>
+  );
+}
---
+++ src/components/typography/SectionTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SectionTitle({ children }) {
+  return (
+    <h2 className="mb-4 text-lg font-semibold text-gray-500">{children}</h2>
+  );
+}
