9750afb35cbcdbbd3cedcb829e1a2f30046de2d7
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-23 21:03:11 +0700

    modified: add routing to the Login & Register page

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,24 @@ import Layout from "./containers/Layout";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Swiper from "./pages/dashboard/Swiper";
 import Information from "./pages/dashboard/Information";
+// ---------------------------------
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* main pages */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="swiper" element={<Swiper />} />
           <Route path="information" element={<Information />} />
         </Route>
+
+        {/* login & register pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
